90fba29253e1289c66e34874a9ffb1cdad1fbbea
isihyeong <buzzcatfish@gmail.com>
2020-09-07 23:15:32 +0900

    init input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Header from './Components/Header';
+import PhoneForm from './Routes/PhoneForm';
 
-function App() {
-    return (
-        <>
-            <Header />
-        </>
-    );
+class App extends React.Component {
+    handleCreate = (data) => {
+        console.log(data);
+    };
+    render() {
+        return (
+            <>
+                <Header />
+                <PhoneForm onCreate={this.handleCreate} />
+            </>
+        );
+    }
 }
 
 export default App;
